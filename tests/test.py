c7f477671bd241ed6131a82b75bce20fd2e2c345
junha6316 <junha6316@gmail.com>
2022-08-17 07:44:53 +0900

    추가: batch 관련 테스트

---
+++ tests/test.py
@@ -0,0 +1,15 @@
+
+# batch를 위한 테스트
+from datetime import date
+
+from ..domain.orderline import OrderLine
+from ..domain.batch import Batch
+
+
+def test_할당시_사용가능한_양_감소():
+    batch = Batch("batch-001", "SMALL-TABLE", qty=20, eta=date.today())
+    line = OrderLine('order-ref', 'SMALL-TABLE', 2)
+
+    batch.allocate(line)
+
+    assert batch.available_quantity == 18
